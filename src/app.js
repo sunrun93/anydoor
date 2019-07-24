c0506c635c6c3daa11e9a48d3a526c6a3d66d192
sunrun93 <wangjian9302@163.com>
2019-07-24 22:41:37 +0800

    add open url auto

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const chalk = require('chalk');
 const conf = require('./config/defaultConfig');
 const path = require('path');
 const router = require('./helper/router');
+const openUrl = require('./helper/openUrl');
 
 class Server {
   constructor(config) {
@@ -14,10 +15,11 @@ class Server {
       const filePath = path.join(this.conf.root, req.url);
       router(req, res, filePath, this.conf);
     });
-
+    
     server.listen(this.conf.port, this.conf.hostname, () => {
       const addr = `http://${this.conf.hostname}:${this.conf.port}`;
       console.log(`Server is running at ${chalk.green(addr)}`);
+      openUrl(addr);
     });
   }
 }
